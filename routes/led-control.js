733b074900be4072984f4c6ddfdbad115711002e
mk221093 <miriamkrebs606@gmail.com>
2021-01-04 00:46:10 +0100

    bluetooth
    bluetoothAddress changed to address

--- routes/led-control.js
+++ routes/led-control.js
@@ -7,34 +7,34 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
-	serial.on('found', function (address, name) {
+    serial.on('found', function (address, name) {
 
-		// you might want to check the found address with the address of your
-		// bluetooth enabled Arduino device here.
+        // you might want to check the found address with the address of your
+        // bluetooth enabled Arduino device here.
 
-		serial.findSerialPortChannel(address, function (channel) {
-			serial.connect(bluetoothAddress, channel, function () {
-				console.log('connected');
-				process.stdin.resume();
-				process.stdin.setEncoding('utf8');
-				console.log('Press "1" or "0" and "ENTER" to turn on or off the light.')
+        serial.findSerialPortChannel(address, function (channel) {
+            serial.connect(address, channel, function () {
+                console.log('connected');
+                process.stdin.resume();
+                process.stdin.setEncoding('utf8');
+                console.log('Press "1" or "0" and "ENTER" to turn on or off the light.')
 
-				process.stdin.on('data', function (data) {
-					serial.write(data);
-				});
+                process.stdin.on('data', function (data) {
+                    serial.write(data);
+                });
 
-				serial.on('data', function (data) {
-					console.log('Received: ' + data);
-				});
-			}, function () {
-				console.log('cannot connect');
-			});
-		});
-	});
+                serial.on('data', function (data) {
+                    console.log('Received: ' + data);
+                });
+            }, function () {
+                console.log('cannot connect');
+            });
+        });
+    });
 
-	serial.inquire();
+    serial.inquire();
 
-	res.send("bluetooth page");
+    res.send("bluetooth page");
 });
 
 module.exports = router;
