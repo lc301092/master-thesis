d1c40fa6383dc839b2ba76ed4b4f04b772f1aa69
mk221093 <miriamkrebs606@gmail.com>
2021-03-14 19:22:01 +0100

    tiled map changes (WIP)
    Prøver at lave eget map med tiled scener. - problemer med at køre scenen i den her version.

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-	res.render('part10');
+	res.render('tilemap');
 });
 
 module.exports = router;
