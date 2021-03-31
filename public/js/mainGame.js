946bbc1b223bdb68c8ac75075f5697b9ee39a510
Luca Casa <lc301092@gmail.com>
2021-03-31 15:28:18 +0200

    Revert "time machine feedback"
    This reverts commit 4d9a36963834c569820f7c100132d87a43e186d6.

--- public/js/mainGame.js
+++ public/js/mainGame.js
@@ -3,15 +3,17 @@
 import {LoadScene} from './scenes/LoadScene.js'; 
 import {MenuScene} from './scenes/MenuScene.js';
 import {Base} from './scenes/BaseScene.js';
+import {TraningScene} from './scenes/TraningScene.js';
 import {ChemistLevel} from './scenes/chemistLvl.js';
 import {Timeline} from './scenes/TimelineScene.js';
 import {TimeMachineScene } from './scenes/TimeMachineScene.js';
+import {TrainingsimulatorScene} from './scenes/TrainingsimulatorScene.js';
 
 
 let game = new Phaser.Game({
     width: 800,
     height: 600,
-    scene: [LoadScene, MenuScene, Base, ChemistLevel, Timeline, TimeMachineScene], 
+    scene: [LoadScene, MenuScene, Base, TraningScene, ChemistLevel, Timeline, TimeMachineScene, TrainingsimulatorScene], 
     physics: {
         default: 'arcade',
         arcade: {
--- public/js/scenes/TimeMachineScene.js
+++ public/js/scenes/TimeMachineScene.js
@@ -1,12 +1,13 @@
 import { constants } from "../constants.js"
-let playerPosition = { x: 150, y: 150 };
+let playerPosition = {x: 150,y: 150};
 let playerProgression;
-let btnX, btnXCircle, btnXValue;
+let btnXCircle;
+let btnXValue;
 let redButtonCircle;
 let titleText;
 let displayFunction;
 let displayUnits;
-let compupterStyle = { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: 'black', fontSize: 24 };
+let compupterStyle = { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: 'black', fontSize: 24};
 
 
 
@@ -17,81 +18,71 @@ export class TimeMachineScene extends Phaser.Scene {
         })
     }
     init(data) {
-        if (data.playerPosition) playerPosition = data.playerPosition;
+        if(data.playerPosition) playerPosition = data.playerPosition;    
 
         console.log(playerPosition);
         playerProgression = JSON.parse(localStorage.getItem('objectives'));
         btnXValue = 0;
-
+        
     }
     preload() {
         //this.player = this.add.
         // tilemaps
     }
     create() {
-        this.add.image(50, 50, 'bg_time_device.png').setOrigin(0);
-        btnX = this.add.image(259, 416, constants.IMAGES.BTN_TIME_MACHINE);
-        btnXCircle = this.add.circle(btnX.x - 2, btnX.y + 5, 50
-           // , 0xff0000, 0.5
-            ).setInteractive({ useHandCursor: true });
-        btnXCircle.on('pointerdown', promptXValue, this);
-        let screen = this.add.image(150, 200, 'clear_screen.png').setOrigin(0).setScale(0.3, 0.2);
-        titleText = this.add.text(255, 85, 'Tidsindstilling', { fontSize: 30, color: 'black' });
+     this.add.image(50,50,constants.IMAGES.BG_TIME_MACHINE).setOrigin(0); 
+     titleText = this.add.text(255,85,'Tidsindstilling',{fontSize:30,color: 'black'});
+     
+     redButtonCircle = this.add.circle(458,180,117).setOrigin(0).setInteractive({useHandCursor: true});
+     redButtonCircle.on('pointerdown', checkTimeSettings,this);
 
-        redButtonCircle = this.add.circle(458, 180, 117).setOrigin(0).setInteractive({ useHandCursor: true });
-        redButtonCircle.on('pointerdown', checkTimeSettings, this);
+     displayFunction = this.add.text(160, 300, 'f(x) = -15x + 2200', compupterStyle);
+     displayUnits = this.add.text(180,385,'x     y      a     b', compupterStyle);
 
-        //  displayFunction = this.add.text(160, 300, 'f(x) = -15x + 2200', compupterStyle);
-        // displayUnits = this.add.text(180,38  5,'x     y      a     b', compupterStyle);
-        const backBtn = this.add.text(50, 550, 'Tilbage', { fill: '#0f0' });
-        backBtn.setInteractive({ useHandCursor: true });
-        backBtn.on('pointerover', () => {
-            backBtn.setStyle({ fontSize: '18px' });
+     btnXCircle = this.add.circle(125,140,50).setOrigin(0).setInteractive({useHandCursor: true});
+     btnXCircle.on('pointerdown',promptXValue,this);
+     const backBtn = this.add.text(50, 550, 'Tilbage', { fill: '#0f0' });    
+     backBtn.setInteractive({ useHandCursor: true });
+     backBtn.on('pointerover', () => {
+        backBtn.setStyle({ fontSize: '18px' });
         });
         backBtn.on('pointerout', () => {
             backBtn.setStyle({ fontSize: '16px' });
         });
 
         backBtn.on('pointerdown', () => {
-            this.scene.start(constants.SCENES.PLAY, { playerPosition });
+            this.scene.start(constants.SCENES.PLAY, {playerPosition});
         });
     }
     update() {
-        //btnX.angle += 1;
+
     }
 }
-function checkTimeSettings() {
-    if (playerProgression) return;
+function checkTimeSettings(){
+    if(playerProgression) return;
     let answer = calculateYValue(btnXValue);
-    if (answer == 1930) {
-        this.scene.start(constants.SCENES.CHEMIST, { playerPosition });
+    if(answer == 1930){
+        this.scene.start(constants.SCENES.CHEMIST, {playerPosition});
     }
-    else {
+    else{
         alert('Der sker ikke noget, der må være noget galt med tidsindstillingen');
     }
 }
-function promptXValue() {
-
-    let promptVal = prompt('\nDrejeknappen står på: ' + btnXValue + '\n\nDu kan dreje og denne knap. Indtast et positivt tal for at dreje den et hak med urets retning eller et negativt tal for at dreje den et hak mod urets retning');
-    if (!promptVal) return;
-
-    if (isNaN(promptVal)) {
+function promptXValue(){
+    
+    let promptVal = prompt('\nDrejeknappen står på: '+ btnXValue +'\n\nDu kan dreje og denne knap. Indtast et positivt tal for at dreje den et hak med urets retning eller et negativt tal for at dreje den et hak mod urets retning');
+    if(!promptVal) return;
+    
+    if(isNaN(promptVal)) {
         alert('du kan kun skrive et tal, prøv igen');
         promptXValue();
     }
     btnXValue += parseInt(promptVal);
-    rotateBtnX(btnXValue);
     // update 
     console.log(btnXValue);
 }
-function calculateYValue(xValue) {
-    let yValue = (-15 * xValue) + 2200;
+function calculateYValue(xValue){
+    let yValue = (-15*xValue)+2200;
     console.log(yValue);
     return yValue;
 };
-
-function rotateBtnX(btnValue) {
-    let rotationInterval = 45;
-    let newRotation = (rotationInterval * btnValue);
-    btnX.angle = newRotation;
-}
